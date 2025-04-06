854f0c8fe89cd88732da9716e977e486624a2da4
RaulRodriguesss <santos.raulrodr@gmail.com>
2025-04-06 14:32:10 -0300

    first commit

---
+++ src/test/java/com/raul/mongo/MongoDbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raul.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
